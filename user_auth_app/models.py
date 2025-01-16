2ef6c1e7560f6e06a2051577ec27bfd077af8b4e
Christoph <c.wagner.germany@gmail.com>
2025-01-16 18:30:15 +0100

    UserProfile2

--- user_auth_app/models.py
+++ user_auth_app/models.py
@@ -3,8 +3,18 @@ from django.db import models
 
 # Create your models here.
 
+
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    ROLE_CHOICES = [
+        ("customer", "Customer"),
+        ("provider", "Provider"),
+    ]
     
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+
     def __str__(self):
         return self.user.username
