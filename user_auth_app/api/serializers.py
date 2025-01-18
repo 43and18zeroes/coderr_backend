e3ad79651d26b431a830f7363b490fe4c194f1a5
Christoph <c.wagner.germany@gmail.com>
2025-01-18 19:20:46 +0100

    UserRegistrationView

--- user_auth_app/api/serializers.py
+++ user_auth_app/api/serializers.py
@@ -2,6 +2,25 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 from user_auth_app.models import UserProfile
 
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    role = serializers.ChoiceField(choices=UserProfile.ROLE_CHOICES)
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'email', 'role']
+        extra_kwargs = {
+            'password': {'write_only': True},
+        }
+
+    def create(self, validated_data):
+        role = validated_data.pop('role', 'customer')
+        password = validated_data.pop('password')
+        user = User.objects.create(**validated_data)
+        user.set_password(password)
+        user.save()
+        UserProfile.objects.create(user=user, role=role)
+        return user
+
 class CustomLoginSerializer(serializers.Serializer):
     email = serializers.EmailField(required=True)
     password = serializers.CharField(write_only=True, style={'input_type': 'password'})
--- user_auth_app/api/views.py
+++ user_auth_app/api/views.py
@@ -1,6 +1,15 @@
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializers import CustomLoginSerializer
+from .serializers import CustomLoginSerializer, UserRegistrationSerializer
+
+class UserRegistrationView(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "User registered successfully."}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 # class CustomLoginView(ObtainAuthToken):
 class CustomLoginView(APIView):
