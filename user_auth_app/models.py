fede43472a9002ffb4653e15c23ec2761870d6aa
Christoph <c.wagner.germany@gmail.com>
2025-01-20 23:09:42 +0100

    UserProfile

--- user_auth_app/models.py
+++ user_auth_app/models.py
@@ -10,7 +10,14 @@ class UserProfile(models.Model):
     ]
     
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    file = models.CharField(max_length=30, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    tel = models.CharField(max_length=30, blank=True)
+    description = models.TextField(max_length=255, blank=True)
+    working_hours = models.CharField(max_length=30, blank=True)
     type = models.CharField(max_length=10, choices=TYPE_CHOICES, default="customer")
+    
 
     def __str__(self):
         return f"{self.user.username} ({self.type})"
