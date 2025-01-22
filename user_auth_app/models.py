d3f47f80118326de3ffd0f9be51595644bb24572
Christoph <c.wagner.germany@gmail.com>
2025-01-22 17:07:53 +0100

    models

--- user_auth_app/models.py
+++ user_auth_app/models.py
@@ -3,25 +3,25 @@ from django.db import models
 
 # Create your models here.
 
+
 class UserProfile(models.Model):
     TYPE_CHOICES = [
         ("customer", "Customer"),
         ("business", "Business"),
     ]
-    
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    
+
     file = models.CharField(max_length=30, blank=True)
     location = models.CharField(max_length=30, blank=True)
     tel = models.CharField(max_length=30, blank=True)
     description = models.TextField(max_length=255, blank=True)
     working_hours = models.CharField(max_length=30, blank=True)
     type = models.CharField(max_length=10, choices=TYPE_CHOICES, default="customer")
-    
 
     def __str__(self):
         return f"{self.user.username} ({self.type})"
-    
+
     def clean(self):
         super().clean()
         if self.type not in dict(self.TYPE_CHOICES).keys():
