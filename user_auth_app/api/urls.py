df4c53191eba2ef7dba7754000d3521eb10cf8ac
Christoph <c.wagner.germany@gmail.com>
2025-01-23 15:41:10 +0100

    new urls

--- user_auth_app/api/urls.py
+++ user_auth_app/api/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
-from .views import CustomLoginView, UserRegistrationView, UserProfileDetailView
+# from django.urls import path
+# from .views import CustomLoginView, UserRegistrationView, UserProfileDetailView
 
-urlpatterns = [
-    path('login/', CustomLoginView.as_view(), name='login'),
-    path('registration/', UserRegistrationView.as_view(), name='user-registration'),
-    path('profile/<int:pk>/', UserProfileDetailView.as_view(), name='user-profile-detail'),
-]
+# urlpatterns = [
+#     path('login/', CustomLoginView.as_view(), name='login'),
+#     path('registration/', UserRegistrationView.as_view(), name='user-registration'),
+#     path('profile/<int:pk>/', UserProfileDetailView.as_view(), name='user-profile-detail'),
+# ]
