fb00728955ac24531320f542dbf9932b8fbe2c63
Christoph <c.wagner.germany@gmail.com>
2025-01-24 17:31:55 +0100

    reviews_app model

--- coderr/settings.py
+++ coderr/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'offers_app',
+    'reviews_app',
     'user_auth_app',
 ]
 
--- reviews_app/models.py
+++ reviews_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Review(models.Model):
+    business_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reviews_received")
+    reviewer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reviews_given")
+    rating = models.PositiveIntegerField()
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Review {self.id} - Rating: {self.rating}"
