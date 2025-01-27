84a294ea8217fd8d3e0f4b19db7bd2ea809d306c
Christoph <c.wagner.germany@gmail.com>
2025-01-27 17:36:50 +0100

    OfferListCreateAPIView pagination

--- offers_app/api/views.py
+++ offers_app/api/views.py
@@ -1,3 +1,4 @@
+from rest_framework.pagination import PageNumberPagination
 from .serializers import OfferSerializer, OfferDetailSerializer
 from django_filters.rest_framework import DjangoFilterBackend, FilterSet, CharFilter
 from offers_app.models import Offer, OfferDetail
@@ -5,13 +6,21 @@ from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.generics import ListCreateAPIView, RetrieveAPIView
 
 
+# Custom Pagination Class
+class OfferPagination(PageNumberPagination):
+    page_size = 10  # Anzahl der Objekte pro Seite
+
+
+# Filter für Offers
 class OfferFilter(FilterSet):
     creator_id = CharFilter(field_name="user__id")
 
     class Meta:
         model = Offer
         fields = ['creator_id', 'user', 'min_delivery_time', 'max_delivery_time']
-        
+
+
+# Offer ListCreateAPIView mit Pagination
 class OfferListCreateAPIView(ListCreateAPIView):
     queryset = Offer.objects.all()
     serializer_class = OfferSerializer
@@ -19,7 +28,10 @@ class OfferListCreateAPIView(ListCreateAPIView):
     filterset_class = OfferFilter
     ordering_fields = ['created_at', 'title']
     search_fields = ['title', 'description']
-    
+    pagination_class = OfferPagination  # Nur hier Pagination aktivieren
+
+
+# Offer Detail View ohne Pagination
 class OfferDetailRetrieveAPIView(RetrieveAPIView):
     queryset = OfferDetail.objects.all()
     serializer_class = OfferDetailSerializer
