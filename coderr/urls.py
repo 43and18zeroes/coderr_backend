a655ff79ee7753ab2ca8d389f7819c7a661cea75
Christoph <c.wagner.germany@gmail.com>
2025-01-28 15:59:17 +0100

    OrderCountView

--- coderr/urls.py
+++ coderr/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from offers_app.api.views import OfferListCreateAPIView, OfferDetailAPIView
-from orders_app.api.views import OrderListCreateView
+from orders_app.api.views import OrderListCreateView, OrderCountView
 from reviews_app.api.views import ReviewListView
 from user_auth_app.api.views import CustomLoginView, UserRegistrationView, UserProfileDetailView, ProfileByTypeListView
 
@@ -28,6 +28,7 @@ urlpatterns = [
     path('api/offers/', OfferListCreateAPIView.as_view(), name='offer-list-create'),
     path('api/offers/<int:pk>/', OfferDetailAPIView.as_view(), name='offer-detail'),
     path('api/orders/', OrderListCreateView.as_view(), name='order-list'),
+    path('api/order-count/<int:business_user_id>/', OrderCountView.as_view(), name='order-count'),
     path('api/profile/<int:pk>/', UserProfileDetailView.as_view(), name='user-profile-detail'),
     path('api/registration/', UserRegistrationView.as_view(), name='user-registration'),
     path('api/reviews/', ReviewListView.as_view(), name='review-list'),
--- orders_app/api/views.py
+++ orders_app/api/views.py
@@ -1,7 +1,17 @@
 from rest_framework.generics import ListCreateAPIView
 from .serializers import OrderSerializer
 from orders_app.models import Order
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 class OrderListCreateView(ListCreateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    
+class OrderCountView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, business_user_id, *args, **kwargs):
+        order_count = Order.objects.filter(business_user_id=business_user_id).count()
+        return Response({"order_count": order_count})
