deac8d356792835e94260319d7dd8e833a353ed1
Christoph <c.wagner.germany@gmail.com>
2025-01-28 16:33:40 +0100

    Business user not found

--- orders_app/api/views.py
+++ orders_app/api/views.py
@@ -4,6 +4,7 @@ from orders_app.models import Order
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.contrib.auth.models import User
 
 class OrderListCreateView(ListCreateAPIView):
     queryset = Order.objects.all()
@@ -13,5 +14,9 @@ class OrderCountView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request, business_user_id, *args, **kwargs):
-        order_count = Order.objects.filter(business_user_id=business_user_id).count()
+        
+        if not User.objects.filter(id=business_user_id).exists():
+            return Response({"error": "Business user not found."}, status=404)
+        
+        order_count = Order.objects.filter(business_user_id=business_user_id, status='in_progress').count()
         return Response({"order_count": order_count})
