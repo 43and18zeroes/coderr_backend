075c9a72f6258f34fd66f6f539d58dc690e68808
Christoph <c.wagner.germany@gmail.com>
2025-02-04 16:57:22 +0100

    offer_count

--- base_info_app/api/views.py
+++ base_info_app/api/views.py
@@ -1,11 +1,25 @@
+from django.db import models
 from rest_framework.views import APIView
 from base_info_app.models import BaseInfo
 from .serializers import BaseInfoSerializer
 from rest_framework.response import Response
+from reviews_app.models import Review
+from user_auth_app.models import UserProfile
+from offers_app.models import Offer
 
 
 class BaseInfoView(APIView):
     def get(self, request, *args, **kwargs):
-        stats, created = BaseInfo.objects.get_or_create(id=1)  # Sicherstellen, dass ein Datensatz existiert
-        serializer = BaseInfoSerializer(stats)
-        return Response(serializer.data)
+        review_count = Review.objects.count()  # Zählt alle Reviews
+        average_rating = Review.objects.aggregate(models.Avg('rating'))['rating__avg'] or 0.0  # Durchschnittswert der Bewertungen
+        business_profile_count = UserProfile.objects.filter(type="business").count()  # Anzahl Business-Profile
+        offer_count = Offer.objects.count()  # Anzahl der Angebote
+
+        data = {
+            "review_count": review_count,
+            "average_rating": round(average_rating, 2),  # Rundet den Durchschnitt
+            "business_profile_count": business_profile_count,
+            "offer_count": offer_count
+        }
+
+        return Response(data)
