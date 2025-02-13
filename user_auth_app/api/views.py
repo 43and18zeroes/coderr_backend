93d031c83f02cd0b99eaaf434aba4aed4649b3c6
Christoph <c.wagner.germany@gmail.com>
2025-02-13 17:15:22 +0100

    HTTP_400_BAD_REQUEST

--- user_auth_app/api/views.py
+++ user_auth_app/api/views.py
@@ -63,6 +63,12 @@ class CustomLoginView(APIView):
     def post(self, request):
         username = request.data.get("username")
         password = request.data.get("password")
+        
+        if not username or not password:
+            return Response(
+                {"error": "Falsche Anmeldeinformationen oder ungültige Eingabe."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
 
         if self.is_special_user(username, password):
             self.handle_special_user(username, password)
