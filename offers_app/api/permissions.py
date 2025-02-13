02984287b57d237d43351eed55dbb855f936bf9e
Christoph <c.wagner.germany@gmail.com>
2025-02-13 17:46:14 +0100

    comments

--- offers_app/api/permissions.py
+++ offers_app/api/permissions.py
@@ -7,15 +7,12 @@ class IsBusinessUser(BasePermission):
     """
     
     def has_permission(self, request, view):
-        # Allow GET requests for all users
         if request.method in ['GET', 'HEAD', 'OPTIONS']:
             return True
         
-        # Ensure the user is authenticated
         if not request.user or not request.user.is_authenticated:
             return False
         
-        # Check if the user has a UserProfile and is a 'business'
         try:
             return request.user.userprofile.type == "business"
         except UserProfile.DoesNotExist:
