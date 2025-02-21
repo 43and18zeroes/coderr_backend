6ec8a997c5db68706da59ebf3490277c2a5e9f20
Christoph <c.wagner.germany@gmail.com>
2025-02-21 16:24:19 +0100

    OfferPagination

--- offers_app/api/views.py
+++ offers_app/api/views.py
@@ -10,6 +10,7 @@ from rest_framework.permissions import AllowAny
 
 class OfferPagination(PageNumberPagination):
     page_size = 10
+    page_size_query_param = 'page_size'
 
 
 class OfferFilter(FilterSet):
