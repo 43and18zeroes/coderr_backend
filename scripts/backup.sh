046c82230d8183c8f3af0e36503794bc1eca0c68
Christoph <c.wagner.germany@gmail.com>
2025-03-12 16:12:58 +0100

    backup shell

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,14 +1,15 @@
 echo "Database Backup started"
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 BACKUP_NAME="db-backup__`date "+%F__%H:%M"`.db"
-cp $SCRIPT_DIR/../db.db $BACKUP_NAME
+cp $SCRIPT_DIR/../db.sqlite3 $BACKUP_NAME
 echo "Uploading file:  $BACKUP_NAME"
 
 echo "
  verbose
- open BACKUP_SERVER.com
- user USER_NAME PASSWORT
+ open w01c0cb4.kasserver.com
+ user f01748c0 fav0r4!M1nt
  bin
+ cd coderr
  put $BACKUP_NAME
  bye
 " | ftp -n > ftp_$$.log
